d553c0d7c039ab2da35603ad11fa09e5a781d524
hapro955 <hapro955@gamil.com>
2018-04-19 14:05:22 +0700

    add class User implements UserInterface

--- adapter.php
+++ adapter.php
@@ -3,5 +3,13 @@ interface UserInterface {
 	public function setName($name);
 	public function getName();
 }
-
+class User implements UserInterface {
+	private $name;
+	pubic function setName ($name) {
+		$this->name = $name;
+	}
+	public function getName () {
+		return $name;
+	}
+}
 ?>
