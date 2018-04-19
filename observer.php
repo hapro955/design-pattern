27c548dc081dafb33463a3c4cde5fa4a9bd33ff8
hapro955 <hapro955@gamil.com>
2018-04-19 16:03:06 +0700

    create class Account implements Subject

--- observer.php
+++ observer.php
@@ -7,4 +7,14 @@ interface Subject {
 	public function detach(Observer $observer);
 	public function notify(); 
 }
+class Account implements Subject {
+	const LOGIN_SECCESS = 1;
+	const LOGIN_FAILURE = 2; 
+	const LOGIN_IVALID = 3;
+	const EXPIRED = 4;
+
+	private $state;
+	private $storage;
+	private $data;
+}
 ?>
