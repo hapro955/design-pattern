4212a4a9e6086b1a7ba9b030b146043b7c5f592c
hapro955 <hapro955@gamil.com>
2018-04-19 15:56:55 +0700

    add interface Subject

--- observer.php
+++ observer.php
@@ -2,4 +2,9 @@
 interface Observer {
 	public function update();
 }
+interface Subject {
+	public function attach(Observer $observer);
+	public function detach(Observer $observer);
+	public function notify(); 
+}
 ?>
